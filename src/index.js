3325e012e6e2028a655fa9e4bbf11e2e830101a8
Ventomexx <ventomexx@gmail.com>
2023-02-14 00:22:29 +0300

    feat:check task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,19 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let strNull = '0000000000';
+    result = '';
+    function replaceStr (Char) {
+     Char = Char.replace(/\./g, "10").replace(/\-/g, "11");
+     result += strNull.substr(0, strNull.length - Char.length) + Char;
+    }
+    for (let i = 0; i < expr.length; i++) {
+      if (expr[i] == ' ') {result +='**********'}
+    for (let key in MORSE_TABLE) {
+      if (MORSE_TABLE[key] === expr[i]) {replaceStr(key)}
+    }
+    }
+    return result
 }
 
 module.exports = {
